950769de14304c29ffbc047e6dab637df8ee5f4b
Rafael <rsilraf@gmail.com>
2022-06-24 11:01:05 -0300

    Add pandas and fruits macro table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import streamlit
+
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -9,3 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fuit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.dataframe(my_fruit_list)
