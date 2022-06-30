37ebe33b4421cf055e38723f61edafc8e9fdf13b
Rafael <rsilraf@gmail.com>
2022-06-30 12:53:29 -0300

    Add testing snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output as table
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+my_cur = my_cnx.cursor()
+my_cur.execute('select current_user(), current_account(), current_region()')
+my_data_row = my_cur.fetchone()
+streamlit.text('Hello from snowflake:')
+streamlit.text(my_data_row)
